9a55293d144a0d2cd17ae129847bccce4f8dae9f
jagrut14 <50945326+jagrut14@users.noreply.github.com>
2021-06-28 20:33:39 +0530

    done

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import streamlit as st
 from st_aggrid import AgGrid
 import os
 os.environ["MODIN_ENGINE"] = "ray"
-ray.init()
+
+ray.remote(memory=2500 * 1024 * 1024)
 import modin.pandas as pd
 
 
